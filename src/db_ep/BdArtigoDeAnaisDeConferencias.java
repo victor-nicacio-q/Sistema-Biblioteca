f7b1d551df6eaf5753e3dffd5dd98ed80b1ff77c
barbinha <raphaeljpivato@gmail.com>
2019-12-06 03:02:38 -0200

    Inclusão do módulo de Update

--- src/db_ep/BdArtigoDeAnaisDeConferencias.java
+++ src/db_ep/BdArtigoDeAnaisDeConferencias.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 import models.artigoDeAnaisDeConferencias;
 
 // INTO TO 
@@ -67,7 +68,20 @@ public class BdArtigoDeAnaisDeConferencias {
 
 		key.close();
 		String sql = "INSERT INTO artigo_de_periodico (titulo_do_artigo, titulo_do_livro, titulo_do_congresso, editora, autores, ano_de_publicacao, mes, volume, numero, pagina_inicial, pagina_final, idLocPub, tituloPub)"
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				+ "VALUES (" + tituloDoArtigo + ", " 
+				+ tituloDoLivro + ", " 
+				+ tituloDoCongresso + ", " 
+				+ editora + ", " 
+				+ autores + ", "
+				+ anoPublicacao + ", " 
+				+ mes + ", "
+				+ volume + ", "
+				+ numero + ", "
+				+ paginaInicial + ", "
+				+ paginaFinal + ", "
+				+ editoresArtigo + ", "
+				+ idLocPub + ", "
+				+ tituloPub + "); ";
 
 		try {
 			PreparedStatement stmt = this.conn.prepareStatement(sql);
@@ -152,8 +166,118 @@ public class BdArtigoDeAnaisDeConferencias {
 	}
 
 	// UPDATE
-	public void alterarLivro() {
-		// PENSAR A FUNCAO DE ALTERACAO //
+	public void alterarArtigoDeAnaisDeConferencia() {
+		Scanner key = new Scanner(System.in);
+		String tituloDoArtigo;
+		String tituloDoLivro;
+		String tituloDoCongresso;
+		String colunasEscolhidas;
+		String[] colunas;
+		String atributo;
+		String sql = "UPDATE FROM artigo_de_anal_de_conferencia";
+		artigoDeAnalDeConferencia a;
+		boolean[] opcoes = new boolean[13];
+		int contaVirgulas;
+		
+		System.out.println("ALTERACAO DE ARTIGOS DE ANAIS DE CONFERENCIA\n");	
+		System.out.println("Insira a chave da tupla que deseja alterar: ");
+		System.out.println("titulo_do_artigo");
+		tituloDoArtigo = key.nextLine();
+		System.out.println("titulo_do_livro");
+		tituloDoLivro = key.nextLine();
+		System.out.println("titulo_do_congresso");
+		tituloDoCongresso = key.nextLine();
+		
+		a = selectArtigoChave(tituloDoArtigo, tituloDoLivro, tituloDoCongresso);
+		
+		exibirArtigoDeAnalDeConferencia(a);
+	
+		System.out.println("Qual(is) dos seguintes atributos sera(o) alterado(s)? (entrar apenas com o(s) numero(s) da(s) coluna(s) separado(s) por virgula");
+		System.out.println();
+		System.out.println("#1 titulo_do_artigo");
+		System.out.println("#2 titulo_do_livro");
+		System.out.println("#3 titulo_do_congresso");
+		System.out.println("#4 editora");
+		System.out.println("#5 autores");
+		System.out.println("#6 pagina_inicial");
+		System.out.println("#7 pagina_final");
+		System.out.println("#8 volume");
+		System.out.println("#9 numero");
+		System.out.println("#10 pagina_inicial");
+		System.out.println("#11 pagina_final");
+		System.out.println("#12 idLocPub");
+		System.out.println("#13 tituloPub");
+		System.out.println();
+		
+		colunasEscolhidas = key.nextLine();
+		colunas = colunasEscolhidas.split(",[ ]*");
+		contaVirgulas = colunas.length;
+
+		for (String atributo : colunas)
+			opcoes[Integer.parseInt(atributo) - 1] = true;
+		
+		for (int i = 0; i < opcoes.length; i++){
+			if (opcoes[i] == true){
+				System.out.println("Qual o conteudo a ser alterado na coluna " + (i + 1) + "?");
+				
+				atributo = key.nextLine();
+	
+				switch(i){
+					case 0: sql = sql + " SET titulo_do_artigo = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 1: sql = sql + " SET titulo_do_livro = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 2: sql = sql + " SET titulo_do_congresso = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 3: sql = sql + " SET editora = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 4: sql = sql + " SET autores = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 5: sql = sql + " pagina_inicial = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 6: sql = sql + " SET pagina_final = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 7: sql = sql + " SET volume = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 8: sql = sql + " SET numero = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 9: sql = sql + " SET mes = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 10: sql = sql + " SET ano = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 11: sql = sql + " SET idLocPub = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case default: sql = sql + " SET tituloPub = " + atributo;
+				}
+			}
+		}
+		
+		sql = sql + " WHERE titulo_do_artigo = " + a.getTituloArtigo()
+					+ "AND titulo_do_livro = " + a.getTituloPub()
+					+ "AND titulo_do_congresso = " + a.getTituloPeriodico() + "; ";
+					
+		try {
+			conn = bd.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.executeQuery();
+			stmt.close();
+			System.out.println("Atualizacao realizada com sucesso");
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}	
 	}
 
 	// DELETE
@@ -171,5 +295,68 @@ public class BdArtigoDeAnaisDeConferencias {
 		stmt.execute();
 		stmt.close();
 		key.close();
+	}
+	
+	//SELECT TUPLA ESPECIFICA PELA CHAVE
+	public static artigoDeAnaisDeConferencia selectArtigoChave(String tituloDoArtigo, String tituloDoLivro, String tituloDoCongresso){
+		String sql = "SELECT * FROM artigo_de_anais_de_conferencia"
+					+ "WHERE titulo_do_artigo = tituloDoArtigo"
+					+ "AND titulo_do_livro = tituloDoLivro"
+					+ "AND titulo_do_congresso = tituloDoCongresso;";
+
+		artigoDeAnaisDeConferencia a = new artigoDeAnaisDeConferencia();
+
+		try {
+			conn = bd.getConnection();
+
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			a.setTituloArtigo((rs.getString("titulo_do_artigo")));
+			a.setTituloLivro(rs.getString("titulo_do_livro"));
+			a.setTituloCongresso(rs.getString("titulo_do_congresso"));                
+			a.setEditora(rs.getString("editora"));
+			a.setAutores(rs.getString("autores"));
+			a.setAno(rs.getInt("ano_de_publicacao"));
+			a.setMes(rs.getInt("mes"));
+			a.setVolume(rs.getInt("volume"));                
+			a.setNumero(rs.getInt("numero"));                
+			a.setPaginaInicial(rs.getInt("pagina_inicial"));
+			a.setPaginaFinal(rs.getInt("pagina_final"));
+			a.setIdLocPub(rs.getInt("IdLocPub"));
+			a.setTituloPub(rs.getInt("titulo_pub"));
+
+			rs.close();
+			stmt.close();
+
+			return a;
+		} 
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//EXIBE TODAS AS TUPLAS DE UM DETERMINADO ARTIGO DE PERIODICO
+	public static void exibirArtigoDeAnalDeConferencia(artigoDeAnalDeConferencia a){
+		System.out.println("Título do Artigo: " + next.getTituloArtigo());
+		System.out.println("Título do Livro: " + next.getTituloPeriodico());
+		System.out.println("Título do Congresso: " + next.getTituloCongresso());
+		System.out.println("Página inicial: " + next.getPaginaInicial());
+		System.out.println("Página Final: " + next.getPaginaFinal());
+		System.out.println("Numero do volume: " + next.getVolume());
+		System.out.println("Numero: " + next.getNumero());
+		System.out.println("Editora: " + next.getEditora());
+		System.out.println("Mes : " + next.getMes());
+		System.out.println("Ano : " + next.getAno());
+		System.out.println("Autores: " + next.getAutores());
+		System.out.println("Id Local de publicação: " + next.getIdLocPub());
+		System.out.println("Título de publicação: " + next.getTituloPub() + "\n \n");
+	}
+	
+	//CONTROLA A QUANTIDADE DE VIRGULAS DOS ARGUMENTOS DO UPDATE
+	public static void controlaVirgulas(int contaVirgulas, String argumentos){
+		contaVirgulas--;
+		if (contaVirgulas > 0)
+			argumentos = argumentos + ",";
 	}
 }
