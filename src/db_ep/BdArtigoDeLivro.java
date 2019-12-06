fff9525e73ad96fd40d1036953455d86c3eafe70
barbinha <raphaeljpivato@gmail.com>
2019-12-06 03:02:06 -0200

    Inclusão do módulo de Update

--- src/db_ep/BdArtigoDeLivro.java
+++ src/db_ep/BdArtigoDeLivro.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.*;
 import models.artigoDeLivro;
 
 
@@ -66,7 +67,18 @@ public class BdArtigoDeLivro {
         key.close();
 		
 		String sql = "INSERT INTO artigo_de_livro (titulo_do_artigo, titulo_pub, titulo_do_livro, pagina_inicial, pagina_final, capitulo, editora, ano_de_publicacao, titulo_original, autores_do_artigo, editores_do_artigo, IdLocPub)" +
-					"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+					+ "VALUES (" + tituloDoArtigo + ", " 
+					+ tituloPub + ", " 
+					+ tituloDoLivro + ", " 
+					+ paginaInicial + ", " 
+					+ paginaFinal + ", " 
+					+ capitulo + ", " 
+					+ editora + ", " 
+					+ anoPublicacao + ", " 
+					+ tituloOriginal + ", "
+					+ autoresArtigo + ", "
+					+ editoresArtigo + ", "
+					+ idLocPub + "); ";
 		
 		try{
 			PreparedStatement stmt = this.conn.prepareStatement(sql);
@@ -150,7 +162,113 @@ public class BdArtigoDeLivro {
 
 	// UPDATE
 	public void alterarLivro() {
-		// PENSAR A FUNCAO DE ALTERACAO //
+		Scanner key = new Scanner(System.in);
+		String tituloDoArtigo;
+		String tituloPub;
+		String tituloDoLivro;
+		String colunasEscolhidas;
+		String[] colunas;
+		String atributo;
+		String sql = "UPDATE FROM artigo_de_livro";
+		artigoDeLivro a;
+		boolean[] opcoes = new boolean[12];
+		int contaVirgulas;
+		
+		System.out.println("ALTERACAO DE ARTIGOS DE LIVRO\n");	
+		System.out.println("Insira a chave da tupla que deseja alterar: ");
+		System.out.println("titulo_do_artigo");
+		tituloDoArtigo = key.nextLine();
+		System.out.println("titulo_pub");
+		tituloPub = key.nextLine();
+		System.out.println("titulo_do_livro");
+		tituloDoLivro = key.nextLine();
+		
+		a = selectArtigoChave(tituloDoArtigo, tituloPub, tituloDoLivro);
+		
+		exibirArtigoDeLivro(a);
+		
+		System.out.println("Qual(is) dos seguintes atributos sera(o) alterado(s)? (entrar apenas com o(s) numero(s) da(s) coluna(s) separado(s) por virgula");
+		System.out.println();
+		System.out.println("#1 titulo_do_artigo");
+		System.out.println("#2 titulo_pub");
+		System.out.println("#3 titulo_do_livro");
+		System.out.println("#4 pagina_inicial");
+		System.out.println("#5 pagina_final");
+		System.out.println("#6 capitulo");
+		System.out.println("#7 editora");
+		System.out.println("#8 ano_de_publicacao");
+		System.out.println("#9 titulo_original");
+		System.out.println("#10 autores_do_artigo");
+		System.out.println("#11 editores_do_artigo");
+		System.out.println("#12 idLocPub");
+		System.out.println();
+		
+		colunasEscolhidas = key.nextLine();
+		colunas = colunasEscolhidas.split(",[ ]*");
+		contaVirgulas = colunas.length;
+
+		for (String atributo : colunas)
+			opcoes[Integer.parseInt(atributo) - 1] = true;
+		
+		for (int i = 0; i < opcoes.length; i++){
+			if (opcoes[i] == true){
+				System.out.println("Qual o conteudo a ser alterado na coluna " + (i + 1) + "?");
+				
+				atributo = key.nextLine();
+				
+				switch(i){
+					case 0: sql = sql + " SET titulo_do_artigo = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 1: sql = sql + " SET titulo_pub = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 2: sql = sql + " SET titulo_do_livro = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 3: sql = sql + " SET pagina_inicial = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 4: sql = sql + " SET pagina_final = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 5: sql = sql + " SET capitulo = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 6: sql = sql + " SET editora = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 7: sql = sql + " SET ano_de_publicacao = " + Integer.parseInt(atributo);
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 8: sql = sql + " SET titulo_original = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 9: sql = sql + " SET autores_do_artigo = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case 10: sql = sql + " SET editores_do_artigo = " + atributo;
+							controlaVirgulas(contaVirgulas, sql);
+							break;
+					case default: sql = sql + " SET idLocPub = " + Integer.parseInt(atributo);
+				}
+			}
+		}
+		
+		sql = sql + " WHERE titulo_do_artigo = " + a.getTituloArtigo()
+					+ "AND titulo_pub = " + a.getTituloPub()
+					+ "AND titulo_do_livro = " + a.getTituloLivro() + "; ";
+					
+		try {
+			conn = bd.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.executeQuery();
+			stmt.close();
+			System.out.println("Atualizacao realizada com sucesso");
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	//DELETE
@@ -168,5 +286,67 @@ public class BdArtigoDeLivro {
 		stmt.execute();
 		stmt.close();
 		key.close();
+	}
+	
+	//SELECT TUPLA ESPECIFICA PELA CHAVE
+	public static artigoDeLivro selectArtigoChave(String tituloDoArtigo, String tituloPub, String tituloDoLivro){
+		String sql = "SELECT * FROM artigo_de_livro"
+					+ "WHERE titulo_do_artigo = tituloDoArtigo"
+					+ "AND titulo_pub = tituloPub"
+					+ "AND titulo_do_livro = tituloDoLivro;";
+
+		artigoDeLivro a = new artigoDeLivro();
+
+		try {
+			conn = bd.getConnection();
+
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			a.setTituloArtigo((rs.getString("titulo_do_artigo")));
+			a.setTituloPeriodico(rs.getString("titulo_do_livro"));
+			a.setPaginaInicial(rs.getInt("pagina_inicial"));
+			a.setPaginaFinal(rs.getInt("pagina_final"));
+			a.setnVolume(rs.getInt("capitulo"));
+			a.setEditora(rs.getString("editora"));
+			a.setAno(rs.getInt("ano"));
+			a.setMes(rs.getInt("titulo_original"));
+			a.setAutores(rs.getString("autores_do_artigo"));
+			a.setEditores(rs.getString("editores_do_artigo"));
+			a.setIdLocPub(rs.getInt("IdLocPub"));
+			a.setTituloPub(rs.getInt("titulo_pub"));
+
+			rs.close();
+			stmt.close();
+
+			return a;
+		} 
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//EXIBE TODAS AS TUPLAS DE UM DETERMINADO ARTIGO DE LIVRO
+	public static void exibirArtigoDeLivro(artigoDeLivro a){
+		System.out.println("Título do Artigo: " + a.getTituloArtigo());
+		System.out.println("Título de publicação: " + a.getTituloPub());
+		System.out.println("Título do Livro: " + a.getTituloLivro());
+		System.out.println("Página inicial: " + a.getPaginaInicial());
+		System.out.println("Página Final: " + a.getPaginaFinal());
+		System.out.println("Capitulo: " + a.getCapitulo());
+		System.out.println("Editora: " + a.getEditora());
+		System.out.println("Ano : " + a.getAno());
+		System.out.println("Título original: " + a.getTituloOriginal());
+		System.out.println("Autores: " + a.getAutores());
+		System.out.println("Editores: " + a.getEditores());
+		System.out.println("Id Local de publicação: " + a.getIdLocPub());
+		System.out.println();
+	}
+	
+	//CONTROLA A QUANTIDADE DE VIRGULAS DOS ARGUMENTOS DO UPDATE
+	public static void controlaVirgulas(int contaVirgulas, String argumentos){
+		contaVirgulas--;
+		if (contaVirgulas > 0)
+			argumentos = argumentos + ",";
 	}
 }
